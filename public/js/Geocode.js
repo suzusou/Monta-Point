5117f9597ad7db16a7d7d98022261aed60144983
github <github>
2022-12-24 12:45:06 +0900

    変数名変更

--- public/js/Geocode.js
+++ public/js/Geocode.js
@@ -183,59 +183,59 @@ function showResult_localSearch(result) {
 
   if (result.ResultInfo.Count > 0) {
 
-    var titleInfo2 = result.Feature[0].Name + "　：　" + result.Feature[0].Property.Address;
+    var titleInfo_localSearch = result.Feature[0].Name + "　：　" + result.Feature[0].Property.Address;
 
-    var splitLatLng2 = result.Feature[0].Geometry.Coordinates.split(',');
+    var splitLatLng_localSearch = result.Feature[0].Geometry.Coordinates.split(',');
 
-    LatLang2 = new google.maps.LatLng(Number(splitLatLng2[1]), Number(splitLatLng2[0]));
+    latLang_localSearch= new google.maps.LatLng(Number(splitLatLng_localSearch[1]), Number(splitLatLng_localSearch[0]));
 
-    var marker2 = new google.maps.Marker({
+    var marker_localSearch = new google.maps.Marker({
       map: map,           //表示している地図を指定する
-      position: LatLang2, //マーカーの表示位置を設定する
-      title: titleInfo2,
+      position: latLang_localSearch, //マーカーの表示位置を設定する
+      title: titleInfo_localSearch,
     });
 
-    var paySplit2 = window.globalData.localSearch_pay[count_localSearch].split(',');
+    var paySplit_localSearch = window.globalData.localSearch_pay[count_localSearch].split(',');
 
-    var content2 = "";
+    var content_localSearch = "";
 
-    for (var i = 0; i < paySplit2.length; i++) {
-      switch (paySplit2[i]) {
+    for (var i = 0; i < paySplit_localSearch.length; i++) {
+      switch (paySplit_localSearch[i]) {
         case "LINE Pay":
-          content2 += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2FLINE%20Pay.jpg?alt=media&token=0fda58e6-2632-43da-bf39-8592b1b976f0" width="50" height="50" alt="nothing" />'
+          content_localSearch += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2FLINE%20Pay.jpg?alt=media&token=0fda58e6-2632-43da-bf39-8592b1b976f0" width="50" height="50" alt="nothing" />'
           break;
         case "PayPay":
-          content2 += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2FPayPay.jpeg?alt=media&token=d8206fc4-4923-4284-9c58-aa13d24599b8" width="50" height="45" alt="nothing" />'
+          content_localSearch += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2FPayPay.jpeg?alt=media&token=d8206fc4-4923-4284-9c58-aa13d24599b8" width="50" height="45" alt="nothing" />'
           break;
         case "楽天 Pay":
-          content2 += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2F%E6%A5%BD%E5%A4%A9%20Pay.png?alt=media&token=2a875fe0-1436-4f87-a78e-c8b3b6204395" width="50" height="38" alt="nothing" />'
+          content_localSearch += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2F%E6%A5%BD%E5%A4%A9%20Pay.png?alt=media&token=2a875fe0-1436-4f87-a78e-c8b3b6204395" width="50" height="38" alt="nothing" />'
           break;
         case "Bank Pay":
-          content2 += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2FBank%20Pay.jpg?alt=media&token=3565a03a-8c94-489f-a12c-117f6e235ec6" width="50" height="50" alt="nothing" />'
+          content_localSearch += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2FBank%20Pay.jpg?alt=media&token=3565a03a-8c94-489f-a12c-117f6e235ec6" width="50" height="50" alt="nothing" />'
           break;
         case "楽天 Edy":
-          content2 += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2F%E6%A5%BD%E5%A4%A9%20Edy.png?alt=media&token=6459e4ae-5a70-4f95-a792-819b259b946b" width="45" height="45" alt="nothing" />'
+          content_localSearch += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2F%E6%A5%BD%E5%A4%A9%20Edy.png?alt=media&token=6459e4ae-5a70-4f95-a792-819b259b946b" width="45" height="45" alt="nothing" />'
           break;
         case "QUICPay":
-          content2 += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2FQUICPay.png?alt=media&token=e9ec4be9-7092-4df7-b02e-326ded83dfda" width="40" height="40" alt="nothing" />'
+          content_localSearch += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2FQUICPay.png?alt=media&token=e9ec4be9-7092-4df7-b02e-326ded83dfda" width="40" height="40" alt="nothing" />'
           break;
         case "au Pay":
-          content2 += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2Fau%20Pay.png?alt=media&token=4490d2f1-58c0-4d86-8b5a-1f28a52051ec" width="40" height="40" alt="nothing" />'
+          content_localSearch += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2Fau%20Pay.png?alt=media&token=4490d2f1-58c0-4d86-8b5a-1f28a52051ec" width="40" height="40" alt="nothing" />'
           break;
         case "Origami Pay":
-          content2 += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2FOrigami%20Pay.jpg?alt=media&token=fbcd0993-ef0a-45f8-a8c8-7c72ff11475f" width="50" height="50" alt="nothing" />'
+          content_localSearch += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2FOrigami%20Pay.jpg?alt=media&token=fbcd0993-ef0a-45f8-a8c8-7c72ff11475f" width="50" height="50" alt="nothing" />'
           break;
         case "d 払い":
-          content2 += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2Fd%E6%89%95%E3%81%84.jpg?alt=media&token=c28033e3-8950-4225-a9ef-f49dcce9d536" width="38" height="38" alt="nothing" />'
+          content_localSearch += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2Fd%E6%89%95%E3%81%84.jpg?alt=media&token=c28033e3-8950-4225-a9ef-f49dcce9d536" width="38" height="38" alt="nothing" />'
           break;
         case "メルペイ":
-          content2 += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2F%E3%83%A1%E3%83%AB%E3%83%9A%E3%82%A4.jpeg?alt=media&token=56ecc3cc-cfc1-4d34-a695-6266cb547323" width="50" height="45" alt="nothing" />'
+          content_localSearch += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2F%E3%83%A1%E3%83%AB%E3%83%9A%E3%82%A4.jpeg?alt=media&token=56ecc3cc-cfc1-4d34-a695-6266cb547323" width="50" height="45" alt="nothing" />'
           break;
         case "ゆうちょPay":
-          content2 += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2F%E3%82%86%E3%81%86%E3%81%A1%E3%82%87Pay.jpeg?alt=media&token=50193d17-2eb8-419a-ad12-dd8de98abd56" width="82" height="72" alt="nothing" />'
+          content_localSearch += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2F%E3%82%86%E3%81%86%E3%81%A1%E3%82%87Pay.jpeg?alt=media&token=50193d17-2eb8-419a-ad12-dd8de98abd56" width="82" height="72" alt="nothing" />'
           break;
         case "QUOカードPay":
-          content2 += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2FQUO%E3%82%AB%E3%83%BC%E3%83%89Pay.png?alt=media&token=22732de3-19ec-4c0b-be12-82a8647975f6" width="47" height="47" alt="nothing" />'
+          content_localSearch += '<img src="https://firebasestorage.googleapis.com/v0/b/montapoint-7b9dd.appspot.com/o/MontaPoint%2FQUO%E3%82%AB%E3%83%BC%E3%83%89Pay.png?alt=media&token=22732de3-19ec-4c0b-be12-82a8647975f6" width="47" height="47" alt="nothing" />'
           break;
         default:
           console.log("画像が見つかりませんでした");
@@ -243,23 +243,23 @@ function showResult_localSearch(result) {
       }
     }
 
-    var infoWindow2 = new google.maps.InfoWindow({
+    var infoWindow_localSearch = new google.maps.InfoWindow({
 
-      content: content2
-
-    });
-
-    marker2.addListener('mouseover', function (e) {
-
-      // console.log(marker[int].title);
-      infoWindow2.open(map, marker2);
+      content: content_localSearch
 
     });
 
-    marker2.addListener('mouseout', function (e) {
+    marker_localSearch.addListener('mouseover', function (e) {
 
       // console.log(marker[int].title);
-      infoWindow2.close(map, marker2)
+      infoWindow_localSearch.open(map, marker_localSearch);
+
+    });
+
+    marker_localSearch.addListener('mouseout', function (e) {
+
+      // console.log(marker[int].title);
+      infoWindow_localSearch.close(map,marker_localSearch)
 
     });
 
